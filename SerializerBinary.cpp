abee800c17bea151e9962721bbac0c720ede9de6
vladimir.skryabin <skryabinv@yandex.ru>
2023-08-16 14:44:49 +0500

    Binary serializer

--- SerializerBinary.cpp
+++ SerializerBinary.cpp
@@ -1,15 +1,35 @@
 #include "SerializerBinary.h"
+#include "Serializable.h"
 #include <QDataStream>
 #include <QFile>
+#include <QDebug>
 
 namespace qstore {
 
 void SerializerBinary::serialize(const Serializable &serializable,
                                  const QString &filename) {
+    QFile file(filename);
+    if(file.open(QIODevice::WriteOnly)) {
+        QDataStream stream(&file);
+        stream << serializable.saveToMap();        
+        file.close();
+    } else {
+        qWarning() << "Can't write file" << filename;
+    }
 }
 
 void SerializerBinary::deserialize(Serializable &serializable,
                                    const QString &filename) {
+    QFile file(filename);
+    if(file.open(QIODevice::ReadOnly)) {        
+        QDataStream stream(&file);
+        QVariantMap map;
+        stream >> map;
+        serializable.loadFromMap(map);
+        file.close();
+    } else {
+        qWarning() << "Can't read file" << filename;
+    }
 }
 
 } // namespace qstore
